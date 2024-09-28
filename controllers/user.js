da43c4d474eacb803b468f00cac77fb8e6a4817b
karankoder <karankr.das.cse22@iitbhu.ac.in>
2024-09-28 18:19:12 +0530

    cookie deletion issue resolved

--- controllers/user.js
+++ controllers/user.js
@@ -59,6 +59,8 @@ export const logout = (req, res, next) => {
       .status(200)
       .cookie('token', '', {
         expires: new Date(Date.now()),
+        sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'none',
+        secure: process.env.NODE_ENV === 'development' ? false : true,
       })
       .json({
         success: true,
